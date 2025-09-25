466d5eaed45abc2618f1802c237520031ea01e54
mafeZapata <49245929+mafeZapata@users.noreply.github.com>
2025-09-25 16:40:23 -0500

    Updated test configuration and UIBasictest implementation

--- test/UIBasictest.spec.js
+++ test/UIBasictest.spec.js
@@ -1,20 +1,39 @@
- const { test } = require ('@playwright/test');
+ const { test, expect } = require('@playwright/test');
 
-
- test('Browser context playwright test', async ({browser}) =>
+test('@Web Browser Context-Validating Error login', async ({browser})=>
  {
-
-   chrome -plugins/ cookies 
-        const context = await browser.newContext(); 
-        const page = await context.newPage();
-        await page.goto('https://www.google.com/');
-
+   
+      const context = await browser.newContext();
+      const page =  await context.newPage();
+    // page.route('**/*.{jpg,png,jpeg}',route=> route.abort());
+      const userName = page.locator('#username');
+      const signIn = page.locator("#signInBtn");
+      const cardTitles =  page.locator(".card-body a");
+      page.on('request',request=> console.log(request.url()));
+      page.on('response',response=> console.log(response.url(), response.status()));
+      await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+      console.log(await page.title());
+      //css 
+     await userName.fill("rahulshetty");
+     await page.locator("[type='password']").fill("learning");
+     await signIn.click();
+    console.log(await page.locator("[style*='block']").textContent());
+    await expect(page.locator("[style*='block']")).toContainText('Incorrect');
+    //type - fill
+    await userName.fill("");
+    await userName.fill("rahulshettyacademy");
+    await signIn.click();
+    console.log(await cardTitles.first().textContent());
+   console.log(await cardTitles.nth(1).textContent());
+   const allTitles = await cardTitles.allTextContents();
+   
+   console.log(allTitles);
+    
 });
 
- 
- test('Page playwright test', async ({page}) =>
- {
+test('Page playwright test', async ({page}) => {
+    await page.goto('https://www.google.com/');
+    console.log(await page.title());
+    await expect(page).toHaveTitle("Google");
 
-        await page.goto('https://www.google.com/');
-
- });
+});
